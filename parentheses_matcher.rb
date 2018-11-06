7cdd58058eab9d29c6a08f2becaaf36cf6a4e00a
brandon <brandon@my-vm.com>
2018-11-06 15:59:00 -0500

    add first passing test and even_parens method

--- parentheses_matcher.rb
+++ parentheses_matcher.rb
@@ -1,5 +1,12 @@
 class ParenthesesMatcher
   def self.match(str, idx)
-    # TODO
+    if even_parens?(str)
+      other_index = idx + str.length / 2
+      other_index
+    end
+  end
+
+  def self.even_parens?(str)
+    str.scan(')').count == str.scan('(').count
   end
 end
